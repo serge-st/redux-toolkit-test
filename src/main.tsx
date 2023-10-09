15adc48c138b7a512edb2dc835fe8403086ae661
Serge Stecenko <sergejs.stecenko@gmail.com>
2023-10-09 15:01:26 +0300

    feat: configure store, add user slice

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
-import { store } from "./app/store"
+import { setupStore } from "./store/store"
 import App from "./App"
 import "./index.css"
+
+const store = setupStore()
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
